e4719c1cdc8d6d7f1c6d24dcb035bb3411a991c4
Oscar Anchondo <oanchondo@g.harvard.edu>
2020-07-06 10:25:12 -0400

    added lab2 features

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
 from flask import Flask, render_template
+import joblib as joblib
+
 
 app = Flask(__name__)
 
+# Load ML model
+model = joblib.load('notebooks/train_test_split.pkl')
+model2 = joblib.load('notebooks/decision_tree.pkl')
 
 @app.route('/')
 def index():
-    bestClassEver = 'Best Class Ever'
-    return render_template('index.html', bCE=bestClassEver)
+
+    # Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
+	prediction = model.predict([[3, 2, 2000, 70, 10903.0, 1]])[0][0].round(1)
+	prediction = str(prediction)
+	# Make prediction - features = ['BEDS', 'BATHS', 'SQFT', 'AGE', 'LOTSIZE', 'GARAGE']
+	prediction2 = model2.predict([[3, 2, 2000, 70, 10903.0, 1]])[0][0].round(1)
+	prediction2 = str(prediction2)
+	return render_template('index.html', pred=prediction, pred2=prediction2)
+
 
 
 @app.route('/world')
@@ -16,4 +28,5 @@ def hello_world():
 
 @app.route('/<you>')
 def hello_you(you):
+
     return f'Hello, {you}!'
